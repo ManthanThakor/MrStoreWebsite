072fb18db9c98d71681ddd187425d7e86a44a9e2
Manthan Thakor <thakormanthan849@gmail.com>
2024-06-25 15:58:19 +0530

    Mr store website : Product section

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,7 +1,7 @@
 import "./Home.scss";
 import Banner from "./Banner/Banner";
 import Category from "./Category/Category";
-
+import Products from "../Products/Products";
 const Home = () => {
   return (
     <>
@@ -10,6 +10,7 @@ const Home = () => {
         <div className="main-content">
           <div className="layout">
             <Category />
+            <Products />
           </div>
         </div>
       </div>
