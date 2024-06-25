f6a622a019de06a7c0fae2334ae77560eff4ea7b
Manthan Thakor <thakormanthan849@gmail.com>
2024-06-25 15:13:20 +0530

    Mr store website : Footer section 1

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -12,13 +12,46 @@ const Footer = () => {
             <div className="title">About</div>
             <div className="text">
               Voluptatem accusantium doloremque laudantium, totam rem aperiam,
-              eaque ipsa quae ab illo inventore veritatis et
+              eaque ipsa quae ab illo inventore veritatis et quasi architecto
+              beatae vitae dicta sunt explicabo eaque ipsa quaeab illo.
             </div>
-            quasi architecto beatae vitae dicta sunt explicabo eaque ipsa quae
-            ab illo.
           </div>
-          <div className="col">col2</div> <div className="col">col3</div>{" "}
-          <div className="col">col4</div>{" "}
+          <div className="col">
+            <div className="title">contact</div>
+            <div className="c-item">
+              <FaLocationArrow />
+              <div className="text">
+                Building Number 10, Kalita Street, 36 Sukriti Society,
+                TanujaGunj, Noida, Sikkim, 334273.
+              </div>
+            </div>
+            <div className="c-item">
+              <FaMobileAlt />
+              <div className="text">Phone: 0371 280 0295</div>
+            </div>
+            <div className="c-item">
+              <FaEnvelope />
+              <div className="text">Email: MrStore@gmail.com</div>
+            </div>
+          </div>
+          <div className="col">
+            <div className="title">Categories</div>
+            <span className="text">Headphones</span>
+            <span className="text">Smart Watches</span>
+            <span className="text">Bluetooth Speakers</span>
+            <span className="text">Wireless Earbuds</span>
+            <span className="text">Home Theatre</span>
+            <span className="text">Projectors</span>
+          </div>
+          <div className="col">
+            <div className="title">Pages</div>
+            <span className="text">Home</span>
+            <span className="text">About</span>
+            <span className="text">Privacy Policy</span>
+            <span className="text">Returns</span>
+            <span className="text">Terms & Conditions</span>
+            <span className="text">Contact us</span>
+          </div>
         </div>
       </footer>
     </>
