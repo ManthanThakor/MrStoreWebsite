6b2b7553d8cfd78c3cbf9d60a7a218bd02dc7af7
Manthan Thakor <thakormanthan849@gmail.com>
2024-06-25 16:10:09 +0530

    Mr store website : Product section card 1

--- client/src/components/Products/Product/Product.jsx
+++ client/src/components/Products/Product/Product.jsx
@@ -1,6 +1,20 @@
 import "./Product.scss";
+import prod from "../../../assets/products/earbuds-prod-1.webp";
+
 const Product = () => {
-    return <div>Product</div>;
+  return (
+    <>
+      <div className="product-card">
+        <div className="thumbnail">
+          <img src={prod} alt="" />
+        </div>
+        <div className="prod-details">
+          <span className="name">Product name</span>
+          <span className="price">&#8377;499</span>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Product;
