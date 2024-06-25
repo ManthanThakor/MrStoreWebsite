28ead2fffb74db48175ac0457846d346b13da068
Manthan Thakor <thakormanthan849@gmail.com>
2024-06-25 14:59:21 +0530

    Mr store website : Footer section

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,12 +1,27 @@
+import React from "react";
 import "./Footer.scss";
-
+import { FaLocationArrow, FaMobileAlt, FaEnvelope } from "react-icons/fa";
+import Payment from "../../assets/payments.png";
 
 const Footer = () => {
-    return (
+  return (
     <>
-    
+      <footer className="footer">
+        <div className="footer-content">
+          <div className="col">
+            <div className="title">About</div>
+            <div className="text">
+              Voluptatem accusantium doloremque laudantium, totam rem aperiam,
+              eaque ipsa quae ab illo inventore veritatis et
+            </div>
+            quasi architecto beatae vitae dicta sunt explicabo eaque ipsa quae
+            ab illo.
+          </div>
+          <div className="col">col2</div> <div className="col">col3</div>{" "}
+          <div className="col">col4</div>{" "}
+        </div>
+      </footer>
     </>
-    )
+  );
 };
-
 export default Footer;
