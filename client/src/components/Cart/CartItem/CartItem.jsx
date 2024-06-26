8b1043d3ac47da82047007fc0de8194298f59340
Manthan Thakor <thakormanthan849@gmail.com>
2024-06-26 10:57:41 +0530

    Mr store website : Cartitem  section 4

--- client/src/components/Cart/CartItem/CartItem.jsx
+++ client/src/components/Cart/CartItem/CartItem.jsx
@@ -1,6 +1,27 @@
+import { MdClose } from "react-icons/md";
 import "./CartItem.scss";
+import prod from "../../../assets/products/earbuds-prod-1.webp";
 const CartItem = () => {
-    return <div>Cart item</div>;
+  return (
+    <>
+      <div className="card-products">
+        <div className="card-product">
+          <div className="img-container">
+            <img src={prod} alt="" />
+          </div>
+          <div className="prod-details">
+            <span className="name">Product Name</span>
+            <MdClose className="close-btn" />
+            <div className="quantity-buttons">
+              <span>-</span>
+              <span>5</span>
+              <span>+</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default CartItem;
