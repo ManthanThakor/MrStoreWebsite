c84d0763ce588c0e2c2a683ec127056098d23b6a
Manthan Thakor <thakormanthan849@gmail.com>
2024-06-26 11:21:26 +0530

    Mr store website : Cartitem  section 7

--- client/src/components/Cart/CartItem/CartItem.jsx
+++ client/src/components/Cart/CartItem/CartItem.jsx
@@ -1,31 +1,33 @@
-import { MdClose } from "react-icons/md";
-import "./CartItem.scss";
-import prod from "../../../assets/products/earbuds-prod-1.webp";
+import React from 'react';
+import { MdClose } from 'react-icons/md';
+import './CartItem.scss';
+import prod from '../../../assets/products/earbuds-prod-1.webp';
+
 const CartItem = () => {
   return (
-    <>
-      <div className="card-products">
-        <div className="card-product">
-          <div className="img-container">
-            <img src={prod} alt="" />
-          </div>
-          <div className="prod-details">
-            <span className="name">Product Name</span>
+    <div className="card-products">
+      <div className="card-product">
+        <div className="img-container">
+          <img src={prod} alt="Product" />
+        </div>
+        <div className="prod-details">
+          <span className="name">
+            Product Name
             <MdClose className="close-btn" />
-            <div className="quantity-buttons">
-              <span>-</span>
-              <span>5</span>
-              <span>+</span>
-            </div>
-            <div className="text">
-                <span>3</span>                <span>x</span>
-                <span className="highlight">&#8377;324</span>
-
-            </div>
+          </span>
+          <div className="quantity-buttons">
+            <span>-</span>
+            <span>5</span>
+            <span>+</span>
+          </div>
+          <div className="text">
+            <span>3</span>
+            <span>x</span>
+            <span className="highlight">&#8377;324</span>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
