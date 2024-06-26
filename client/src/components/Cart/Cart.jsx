1640860d76ddc703c0b80efaac1f7a08315b9149
Manthan Thakor <thakormanthan849@gmail.com>
2024-06-26 10:02:01 +0530

    Mr store website : Cart section

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -1,6 +1,17 @@
+import { MdClose } from "react-icons/md";
+import { BsCartX } from "react-icons/bs";
+
 import "./Cart.scss";
 const Cart = () => {
-    return <div>Cart</div>;
+  return (
+    <>
+      <div className="cart-panel">
+        <div className="opac-layer"></div>
+        <div className="cart-content"></div>
+      </div>
+      ;
+    </>
+  );
 };
 
 export default Cart;
