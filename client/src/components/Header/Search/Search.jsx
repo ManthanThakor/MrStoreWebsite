b0094cab088050433de619a722f8378841c253a0
Manthan Thakor <thakormanthan849@gmail.com>
2024-06-26 12:11:35 +0530

    Mr store website : Search section 2

--- client/src/components/Header/Search/Search.jsx
+++ client/src/components/Header/Search/Search.jsx
@@ -32,20 +32,8 @@ const Search = ({ setShowsearch }) => {
               <img src={prod} alt="Product" />
             </div>
             <div className="prod-details">
-              <span className="name">
-                Product Name
-                <MdClose className="close-btn" />
-              </span>
-              <div className="quantity-buttons">
-                <span>-</span>
-                <span>5</span>
-                <span>+</span>
-              </div>
-              <div className="text">
-                <span>3</span>
-                <span>x</span>
-                <span className="highlight">&#8377;324</span>
-              </div>
+              <span className="name">Product Name</span>
+              <span className="desc">Product desc</span>
             </div>
           </div>
         </div>
