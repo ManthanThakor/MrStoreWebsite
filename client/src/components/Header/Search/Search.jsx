78f3da467375296a4af1eab7ea299a8f4b0630e8
Manthan Thakor <thakormanthan849@gmail.com>
2024-06-26 12:04:36 +0530

    Mr store website : Search section 1

--- client/src/components/Header/Search/Search.jsx
+++ client/src/components/Header/Search/Search.jsx
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import { MdClose } from 'react-icons/md';
-import './Search.scss';
+import React, { useState, useEffect } from "react";
+import { MdClose } from "react-icons/md";
+import "./Search.scss";
+import prod from "../../../assets/products/earbuds-prod-1.webp";
 
 const Search = ({ setShowsearch }) => {
   const [animateOut, setAnimateOut] = useState(false);
@@ -13,24 +14,40 @@ const Search = ({ setShowsearch }) => {
     if (animateOut) {
       const timer = setTimeout(() => {
         setShowsearch(false);
-      }, 500); 
+      }, 500);
       return () => clearTimeout(timer);
     }
   }, [animateOut, setShowsearch]);
 
   return (
-    <div className={`search-model ${animateOut ? 'slide-out' : ''}`}>
+    <div className={`search-model ${animateOut ? "slide-out" : ""}`}>
       <div className="form-field">
-        <input 
-          type="text" 
-          autoFocus 
-          placeholder="Search for Products" 
-        />
+        <input type="text" autoFocus placeholder="Search for Products" />
         <MdClose onClick={handleClose} />
       </div>
       <div className="search-result-content">
         <div className="search-result">
-            
+          <div className="search-result-item">
+            <div className="img-container">
+              <img src={prod} alt="Product" />
+            </div>
+            <div className="prod-details">
+              <span className="name">
+                Product Name
+                <MdClose className="close-btn" />
+              </span>
+              <div className="quantity-buttons">
+                <span>-</span>
+                <span>5</span>
+                <span>+</span>
+              </div>
+              <div className="text">
+                <span>3</span>
+                <span>x</span>
+                <span className="highlight">&#8377;324</span>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
