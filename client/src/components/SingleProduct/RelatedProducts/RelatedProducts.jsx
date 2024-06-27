04752f3d45a8b57099ff779412b0383d3c29cf91
Manthan Thakor <thakormanthan849@gmail.com>
2024-06-27 09:47:56 +0530

    Mr store website : Related Product 1

--- client/src/components/SingleProduct/RelatedProducts/RelatedProducts.jsx
+++ client/src/components/SingleProduct/RelatedProducts/RelatedProducts.jsx
@@ -1,15 +1,24 @@
+import React from "react";
 import Product from "../../Products/Product/Product";
 import Products from "../../Products/Products";
-import useFetch from "../../hooks/usefetch";
+import useFetch from "../../../hooks/usefetch";
 
-const RelatedProducts = () => {
-  const { data } = useFetch(`/api/products?populate=*&filters[id][$ne]=${productId}&filters[categories][id]=${categoryId}&pagination[start]=0&pagination[limit]=4`);
+
+const RelatedProducts = ({ productId, categoryId }) => {
+  const { data, error } = useFetch(`/api/products?populate=*&filters[id][$ne]=${productId}&filters[categories][id]=${categoryId}&pagination[start]=0&pagination[limit]=4`);
+
+  if (error) {
+    return <div>Error loading related products</div>;
+  }
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <>
-      <div className="related-products">
-        <Products headingText="Related Products" />
-      </div>
-    </>
+    <div className="related-products">
+      <Products headingText="Related Products" products={data} />
+    </div>
   );
 };
 
