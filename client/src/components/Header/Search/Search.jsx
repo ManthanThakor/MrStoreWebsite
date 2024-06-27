f688cdb6daa231cdf41ace33f5b5224019721cb4
Manthan Thakor <thakormanthan849@gmail.com>
2024-06-27 14:16:46 +0530

    Mr store website : Search problem solve

--- client/src/components/Header/Search/Search.jsx
+++ client/src/components/Header/Search/Search.jsx
@@ -1,10 +1,18 @@
 import React, { useState, useEffect } from "react";
 import { MdClose } from "react-icons/md";
 import "./Search.scss";
-import prod from "../../../assets/products/earbuds-prod-1.webp";
+import { useNavigate } from "react-router-dom";
+import usefetch from "../../../hooks/usefetch";
 
 const Search = ({ setShowsearch }) => {
+  const [query, setQuery] = useState("");
   const [animateOut, setAnimateOut] = useState(false);
+  const navigate = useNavigate();
+  const { data } = usefetch(`/api/products?populate=*&filters[title][$contains]=${query}`);
+
+  const onChange = (e) => {
+    setQuery(e.target.value);
+  };
 
   const handleClose = () => {
     setAnimateOut(true);
@@ -22,20 +30,28 @@ const Search = ({ setShowsearch }) => {
   return (
     <div className={`search-model ${animateOut ? "slide-out" : ""}`}>
       <div className="form-field">
-        <input type="text" autoFocus placeholder="Search for Products" />
+        <input type="text" autoFocus placeholder="Search for Products" value={query} onChange={onChange} />
         <MdClose onClick={handleClose} />
       </div>
       <div className="search-result-content">
         <div className="search-result">
-          <div className="search-result-item">
-            <div className="img-container">
-              <img src={prod} alt="Product" />
+          {query.length > 0 && data && data.data.map(item => (
+            <div key={item.id} className="search-result-item" onClick={() => {
+              navigate(`/product/${item.id}`);
+              handleClose(); // Assuming this is intended to be a function call
+            }}>
+              <div className="img-container">
+                <img src={import.meta.env.VITE_DEV_URL + item.attributes.img.data[0].attributes.url} alt="Product" />
+              </div>
+              <div className="prod-details">
+                <span className="name">{item.attributes.title}</span>
+                <span className="desc">{item.attributes.desc}</span>
+              </div>
             </div>
-            <div className="prod-details">
-              <span className="name">Product Name</span>
-              <span className="desc">Product desc</span>
-            </div>
-          </div>
+          ))}
+          {query.length === 0 && (
+            <div className="no-results">Enter a search query to find products.</div>
+          )}
         </div>
       </div>
     </div>
