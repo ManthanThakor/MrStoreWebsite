894ecec8426c3b1500aa2726c99056602c4b8a77
Manthan Thakor <thakormanthan849@gmail.com>
2024-06-27 13:36:25 +0530

    Mr store website : cart add section 1

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -1,3 +1,4 @@
+
 import { MdClose } from "react-icons/md";
 import { BsCartX } from "react-icons/bs";
 import "./Cart.scss";
@@ -6,39 +7,46 @@ import { useContext } from "react";
 import { Context } from "../../utils/context";
 
 const Cart = ({ setShowcart }) => {
- const {cartItems, cartSubTotal} =  useContext(Context)
-  return (
-    <>
-      <div className="cart-panel">
-        <div className="opac-layer"></div>
-        <div className="cart-content">
-          <div className="cart-header">
-            <span className="heading">Shopping Cart</span>
-            <span className="close-btn" onClick={() => setShowcart(false)}>
-              <MdClose />
-              <span className="text">close</span>
-            </span>
-          </div>
+  const { cartItems, cartSubTotal } = useContext(Context);
 
-          {/* <div className="empty-cart">
+  return (
+    <div className="cart-panel">
+      <div className="opac-layer"></div>
+      <div className="cart-content">
+        <div className="cart-header">
+          <span className="heading">Shopping Cart</span>
+          <span className="close-btn" onClick={() => setShowcart(false)}>
+            <MdClose />
+            <span className="text">close</span>
+          </span>
+        </div>
+
+        {!cartItems.length && (
+          <div className="empty-cart">
             <BsCartX />
             <span className="text">No products in the cart.</span>
             <button className="return-cta">RETURN TO SHOP</button>
-          </div> */}
-          <CartItem/>
-          <div className="cart-footer">
-            <div className="subtotal">
-              <span className="text">Subtotal</span>
-              <span className="text total">&#8377;{cartSubTotal}</span>
-            </div>
-            <div className="button">
-            <button className="checkout-cta">CHECKOUT</button>
-            </div>
-            
           </div>
-        </div>
+        )}
+
+        {cartItems.length > 0 && (
+          <>
+            {cartItems.map((item) => (
+              <CartItem key={item.id} item={item} />
+            ))}
+            <div className="cart-footer">
+              <div className="subtotal">
+                <span className="text">Subtotal</span>
+                <span className="text total">&#8377;{cartSubTotal}</span>
+              </div>
+              <div className="button">
+                <button className="checkout-cta">CHECKOUT</button>
+              </div>
+            </div>
+          </>
+        )}
       </div>
-    </>
+    </div>
   );
 };
 
--- client/src/components/Cart/CartItem/CartItem.jsx
+++ client/src/components/Cart/CartItem/CartItem.jsx
@@ -1,40 +1,37 @@
 import { MdClose } from 'react-icons/md';
 import './CartItem.scss';
-import prod from '../../../assets/products/earbuds-prod-1.webp';
 import { useContext } from 'react';
 import { Context } from '../../../utils/context';
 
-const CartItem = () => {
-const { cartItems ,   handleRemoveFromCart ,   handleCartProductQuantity} = useContext(Context);
-   return (
-    <div className="card-products">
-      {cartItems.map(item => (
-        <div key={item.id} className="card-product">
-        <div className="img-container">
-          <img src={prod} alt="Product" />
+const CartItem = ({ item }) => {
+  const { handleRemoveFromCart, handleCartProductQuantity } = useContext(Context);
+  const imageUrl = import.meta.env.VITE_DEV_URL + item.attributes.img.data[0].attributes.url;
+
+  return (
+    <div className="card-product">
+      <div className="img-container">
+        <img src={imageUrl} alt="Product" />
+      </div>
+      <div className="prod-details">
+        <span className="name">
+          {item.attributes.title}
+          <MdClose className="close-btn" onClick={() => handleRemoveFromCart(item)} />
+        </span>
+        <div className="quantity-buttons">
+          <span onClick={() => handleCartProductQuantity("dec", item)}>-</span>
+          <span>{item.attributes.quantity}</span>
+          <span onClick={() => handleCartProductQuantity("inc", item)}>+</span>
         </div>
-        <div className="prod-details">
-          <span className="name">
-{item.attributes.title}
-            <MdClose className="close-btn" onClick={() => handleRemoveFromCart(item)}/>
-          </span>
-          <div className="quantity-buttons">
-            <span onClick={() =>   handleCartProductQuantity("dec",item)}>-</span>
-            <span>{item.attributes.quantity}</span>
-            <span onClick={() =>   handleCartProductQuantity("inc",item)}>+</span>
-          </div>
-          <div className="text">
-            <span>{item.attributes.quantity}</span>
-            <span>x</span>
-            <span className="highlight">&#8377;{item.attributes.price * item.attributes.quantity}</span>
-          </div>
+        <div className="text">
+          <span>{item.attributes.quantity}</span>
+          <span>x</span>
+          <span className="highlight">&#8377;{item.attributes.price * item.attributes.quantity}</span>
         </div>
       </div>
-
-      ))}
-      
     </div>
   );
 };
 
 export default CartItem;
+
+
