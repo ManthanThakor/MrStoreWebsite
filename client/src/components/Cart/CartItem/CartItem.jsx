a727b035c3e2118c072d32c174b25ad7cce81e81
Manthan Thakor <thakormanthan849@gmail.com>
2024-06-27 13:44:46 +0530

    Mr store website : cart add section 2

--- client/src/components/Cart/CartItem/CartItem.jsx
+++ client/src/components/Cart/CartItem/CartItem.jsx
@@ -1,31 +1,44 @@
-import { MdClose } from 'react-icons/md';
-import './CartItem.scss';
-import { useContext } from 'react';
-import { Context } from '../../../utils/context';
+import { MdClose } from "react-icons/md";
+import "./CartItem.scss";
+import { useContext } from "react";
+import { Context } from "../../../utils/context";
 
 const CartItem = ({ item }) => {
-  const { handleRemoveFromCart, handleCartProductQuantity } = useContext(Context);
-  const imageUrl = import.meta.env.VITE_DEV_URL + item.attributes.img.data[0].attributes.url;
+  const { handleRemoveFromCart, handleCartProductQuantity } =
+    useContext(Context);
+  const imageUrl =
+    import.meta.env.VITE_DEV_URL + item.attributes.img.data[0].attributes.url;
 
   return (
-    <div className="card-product">
-      <div className="img-container">
-        <img src={imageUrl} alt="Product" />
-      </div>
-      <div className="prod-details">
-        <span className="name">
-          {item.attributes.title}
-          <MdClose className="close-btn" onClick={() => handleRemoveFromCart(item)} />
-        </span>
-        <div className="quantity-buttons">
-          <span onClick={() => handleCartProductQuantity("dec", item)}>-</span>
-          <span>{item.attributes.quantity}</span>
-          <span onClick={() => handleCartProductQuantity("inc", item)}>+</span>
+    <div className="card-products">
+      <div className="card-product">
+        <div className="img-container">
+          <img src={imageUrl} alt="Product" />
         </div>
-        <div className="text">
-          <span>{item.attributes.quantity}</span>
-          <span>x</span>
-          <span className="highlight">&#8377;{item.attributes.price * item.attributes.quantity}</span>
+        <div className="prod-details">
+          <span className="name">
+            {item.attributes.title}
+            <MdClose
+              className="close-btn"
+              onClick={() => handleRemoveFromCart(item)}
+            />
+          </span>
+          <div className="quantity-buttons">
+            <span onClick={() => handleCartProductQuantity("dec", item)}>
+              -
+            </span>
+            <span>{item.attributes.quantity}</span>
+            <span onClick={() => handleCartProductQuantity("inc", item)}>
+              +
+            </span>
+          </div>
+          <div className="text">
+            <span>{item.attributes.quantity}</span>
+            <span>x</span>
+            <span className="highlight">
+              &#8377;{item.attributes.price * item.attributes.quantity}
+            </span>
+          </div>
         </div>
       </div>
     </div>
@@ -33,5 +46,3 @@ const CartItem = ({ item }) => {
 };
 
 export default CartItem;
-
-
